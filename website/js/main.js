2361947ee494b761b9b45344d635f71e3c4a8fbe
Kevin Roark <ker2143@columbia.edu>
2015-04-21 21:16:21 -0400

    beginning of web content

--- website/js/main.js
+++ website/js/main.js
@@ -1,3 +1,19 @@
 $(function() {
 
+  var headerVideo = document.querySelector('#header-video');
+
+  $('body').mousemove(function(ev) {
+    var x = ev.pageX;
+    var percent = x / window.innerWidth;
+    seekToPercent(headerVideo, percent);
+  });
+
+  function seekToPercent(vid, percent) {
+    var min = 0;
+    var max = vid.duration;
+    var seekLocation = (max - min) * percent;
+
+    vid.currentTime = seekLocation;
+  }
+
 });
